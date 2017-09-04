9665a3e52a20530729ba7ab130c9ebefac27d90c
Hein Htet <heinhtet.hh405@gmail.com>
2017-09-04 15:15:17 +0630

    Second Commit --> Create Dashboard with include footer (only UI Design)

---
+++ dashboard.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Admin Dashboard</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/adminstyles.css">
+        <script src="js/jquery-3.2.1.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-2">
+                    <h1>Sapphire</h1>
+                    <ul id="Side_Menu" class="nav nav-pills nav-stacked">
+                        <li class="active"><a href="Dashboard.php">
+                                <span class="glyphicon glyphicon-th"></span>
+                                &nbsp;Dashboard</a></li>
+                        <li ><a href="#"> <span class="glyphicon glyphicon-list-alt"></span>
+                                &nbsp;Add New Post</a></li>               
+                        <li><a href="#"><span class="glyphicon glyphicon-tags"></span>
+                                &nbsp;Categories</a></li>
+                        <li><a href="#"> <span class="glyphicon glyphicon-user"></span>
+                                &nbsp;Manage Admins</a></li>
+                        <li><a href="#"><span class="glyphicon glyphicon-comment"></span>
+                                &nbsp;Comments</a></li>
+                        <li><a href="#"> <span class="glyphicon glyphicon-equalizer"></span>
+                                &nbsp;Live Blog</a></li>
+                        <li><a href="#"> <span class="glyphicon glyphicon-log-out"></span>
+                                &nbsp;Logout</a></li>
+                    </ul>
+
+                </div> <!-- Ending of Side Area -->
+                <div class="col-sm-10">
+                    <h1>Admin Dashboard</h1>
+                    <p>
+                        There will be a Google Agencies Day Europe on 3rd October, 
+                        2017 at our offices in Kings Cross / London. 
+                        So if you're running a mobile development agency - it's a free event, 
+                        there will be great talks and great exchange between experts.
+                    </p>
+                    <p>
+                        There will be a Google Agencies Day Europe on 3rd October, 
+                        2017 at our offices in Kings Cross / London. 
+                        So if you're running a mobile development agency - it's a free event, 
+                        there will be great talks and great exchange between experts.
+                    </p>
+                    <p>
+                        There will be a Google Agencies Day Europe on 3rd October, 
+                        2017 at our offices in Kings Cross / London. 
+                        So if you're running a mobile development agency - it's a free event, 
+                        there will be great talks and great exchange between experts.
+                    </p>
+                    <p>
+                        There will be a Google Agencies Day Europe on 3rd October, 
+                        2017 at our offices in Kings Cross / London. 
+                        So if you're running a mobile development agency - it's a free event, 
+                        there will be great talks and great exchange between experts.
+                    </p>
+                    <p>
+                        There will be a Google Agencies Day Europe on 3rd October, 
+                        2017 at our offices in Kings Cross / London. 
+                        So if you're running a mobile development agency - it's a free event, 
+                        there will be great talks and great exchange between experts.
+                    </p>
+                    <p>
+                        There will be a Google Agencies Day Europe on 3rd October, 
+                        2017 at our offices in Kings Cross / London. 
+                        So if you're running a mobile development agency - it's a free event, 
+                        there will be great talks and great exchange between experts.
+                    </p>
+                    <p>
+                        There will be a Google Agencies Day Europe on 3rd October, 
+                        2017 at our offices in Kings Cross / London. 
+                        So if you're running a mobile development agency - it's a free event, 
+                        there will be great talks and great exchange between experts.
+                    </p>
+                    <p>
+                        There will be a Google Agencies Day Europe on 3rd October, 
+                        2017 at our offices in Kings Cross / London. 
+                        So if you're running a mobile development agency - it's a free event, 
+                        there will be great talks and great exchange between experts.
+                    </p>
+                    <p>
+                        There will be a Google Agencies Day Europe on 3rd October, 
+                        2017 at our offices in Kings Cross / London. 
+                        So if you're running a mobile development agency - it's a free event, 
+                        there will be great talks and great exchange between experts.
+                    </p>
+                    <p>
+                        There will be a Google Agencies Day Europe on 3rd October, 
+                        2017 at our offices in Kings Cross / London. 
+                        So if you're running a mobile development agency - it's a free event, 
+                        there will be great talks and great exchange between experts.
+                    </p>
+                    <p>
+                        There will be a Google Agencies Day Europe on 3rd October, 
+                        2017 at our offices in Kings Cross / London. 
+                        So if you're running a mobile development agency - it's a free event, 
+                        there will be great talks and great exchange between experts.
+                    </p>
+                    <p>
+                        There will be a Google Agencies Day Europe on 3rd October, 
+                        2017 at our offices in Kings Cross / London. 
+                        So if you're running a mobile development agency - it's a free event, 
+                        there will be great talks and great exchange between experts.
+                    </p>
+                    <p>
+                        There will be a Google Agencies Day Europe on 3rd October, 
+                        2017 at our offices in Kings Cross / London. 
+                        So if you're running a mobile development agency - it's a free event, 
+                        there will be great talks and great exchange between experts.
+                    </p>
+                    <p>
+                        There will be a Google Agencies Day Europe on 3rd October, 
+                        2017 at our offices in Kings Cross / London. 
+                        So if you're running a mobile development agency - it's a free event, 
+                        there will be great talks and great exchange between experts.
+                    </p>
+                </div> <!-- Ending of Main Area -->
+            </div> <!-- Ending of Row -->
+        </div> <!-- Ending of Container -->
+        <div id="Footer">
+            <hr><p>Theme By | Hein Htet |&copy;2017-2020 --- All right reserved.
+            </p>
+            <a style="color: white; text-decoration: none; cursor: pointer; font-weight:bold;" href="http://jazebakram.com/coupons/" target="_blank">
+                <p>
+                    This site is only used for Study purpose jazebakram.com have all the rights. no one is allow to distribute
+                    copies other then <br>&trade; jazebakram.com &trade;  Udemy ; &trade; Skillshare ; &trade; StackSkills</p><hr>
+            </a>
+
+        </div>
+        <div style="height: 10px; background: #27AAE1;"></div> 
+    </body>
+</html>
